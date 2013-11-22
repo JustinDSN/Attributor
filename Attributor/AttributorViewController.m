2e01b74f52ad34711b7bea3852f7a406255054c1
Justin Steffen <JustinDSN@gmail.com>
2013-11-21 23:15:47 -0500

    Adding initial features for Attributor

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -9,6 +9,9 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *header;
+@property (strong, nonatomic) IBOutlet UITextView *body;
+@property (strong, nonatomic) IBOutlet UIButton *outlineButton;
 
 @end
 
@@ -18,12 +21,31 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title =
+        [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    
+    [title setAttributes:@{NSStrokeWidthAttributeName : @3,
+                           NSStrokeColorAttributeName : self.outlineButton.tintColor}
+                   range:NSMakeRange(0, [title length])];
+    
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+- (IBAction)outlineBodySelection {
+    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName : @-3,
+                                           NSStrokeColorAttributeName : [UIColor blackColor]}
+                                   range:self.body.selectedRange];
+}
+- (IBAction)unoutlineBodySelection {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+                                     range:self.body.selectedRange];
+    [self.body.textStorage removeAttribute:NSStrokeColorAttributeName
+                                     range:self.body.selectedRange];
 }
 
 @end
